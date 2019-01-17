704e3225b105de9efcf2d27ec8de45a9c06a2738
michon jordane <jordane.michon@gmail.com>
2019-01-17 23:53:23 +0100

    fix build

--- server/index.js
+++ server/index.js
@@ -8,9 +8,31 @@ import cookieParser from 'cookie-parser';
 import AWS from 'aws-sdk';
 import clientConfig from '../webpack/client.dev';
 import serverConfig from '../webpack/server.dev';
-import config from './config';
 
 const fs = require('fs');
+const Joi = require('joi');
+require('dotenv').config();
+
+const envVarsSchema = Joi.object({
+  AWS_ACCESS_KEY: Joi.string()
+    .required()
+    .description('AWS access key required'),
+  AWS_SECRET_ACCESS_KEY: Joi.string()
+    .required()
+    .description('AWS secret access key required'),
+})
+  .unknown()
+  .required();
+
+const { error, value: envVars } = Joi.validate(process.env, envVarsSchema);
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const config = {
+  AwsAccessKey: envVars.AWS_ACCESS_KEY,
+  AwsAccessSecretKey: envVars.AWS_SECRET_ACCESS_KEY,
+};
 
 const DEV = process.env.NODE_ENV === 'development';
 const port = DEV ? 3000 : 4030;
