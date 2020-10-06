c7524fa4e0be28e2fdfe533ad634de65afb6682f
jordane michon <jordane.michon@gmail.com>
2020-10-06 15:55:30 +0200

    fix delay animation slide

--- src/common/components/Switcher.tsx
+++ src/common/components/Switcher.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
@@ -8,13 +8,28 @@ type Props = {
   children: ReactElement;
 };
 
-const Switcher = ({ page, direction, children }: Props) => (
-  <TransitionGroup className={`switcher ${direction}`} duration={500}>
-    <CSSTransition key={page} timeout={500} classNames="slide">
-      {children}
-    </CSSTransition>
-  </TransitionGroup>
-);
+const Switcher = ({ page, direction, children }: Props) => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShow(true);
+    }, 250);
+
+    return () => {
+      setShow(false);
+      clearTimeout(timeout);
+    };
+  }, [page]);
+
+  return (
+    <TransitionGroup className={`switcher ${direction}`} duration={500}>
+      <CSSTransition key={page} timeout={500} classNames="slide">
+        <>{show ? children : null}</>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
 
 const mapStateToProps = (state: any) => ({
   page: state.page,
